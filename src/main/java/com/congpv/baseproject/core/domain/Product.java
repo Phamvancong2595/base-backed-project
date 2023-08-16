5b2984835219e95fdab7eb7d5d0ec5901b6ee06f
bob <congphma2595@gmail.com>
2023-08-16 22:31:22 +0700

    config redis cache

--- src/main/java/com/congpv/baseproject/core/domain/Product.java
+++ src/main/java/com/congpv/baseproject/core/domain/Product.java
@@ -1,11 +1,15 @@
 package com.congpv.baseproject.core.domain;
 
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product implements Serializable {
     private Long id;
     private String name;
--- src/main/java/com/congpv/baseproject/infrastructure/config/CacheConfig.java
+++ src/main/java/com/congpv/baseproject/infrastructure/config/CacheConfig.java
@@ -1,27 +1,41 @@
 package com.congpv.baseproject.infrastructure.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.lettuce.core.RedisChannelWriter;
 import java.time.Duration;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
-public class CacheConfig {
+public class CacheConfig extends CachingConfigurerSupport implements CachingConfigurer {
   private final int CACHE_REDIS_TTL = 60;
+  public static final String CACHE_REDIS ="CACHE_REDIS";
 
-  @Bean
-  @Primary
-  public RedisCacheConfiguration defaultCacheConfig(ObjectMapper objectMapper) {
+
+  public RedisCacheConfiguration buildCacheConfig(int seconds) {
     return RedisCacheConfiguration.defaultCacheConfig()
-        .entryTtl(Duration.ofSeconds(CACHE_REDIS_TTL))
+        .entryTtl(Duration.ofSeconds(seconds))
         .disableCachingNullValues()
         .serializeValuesWith(
             RedisSerializationContext.SerializationPair.fromSerializer(
-                new GenericJackson2JsonRedisSerializer(objectMapper)));
+                new GenericJackson2JsonRedisSerializer()));
   }
-
+  @Bean
+  public RedisCacheWriter redisChannelWriter(RedisConnectionFactory redisConnectionFactory) {
+    return RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
+  }
+  @Bean(name = CACHE_REDIS)
+  @Primary
+  public RedisCacheManager redisCacheManager(RedisCacheWriter redisCacheWriter) {
+    return new RedisCacheManager(redisCacheWriter, buildCacheConfig(CACHE_REDIS_TTL));
+  }
 }
