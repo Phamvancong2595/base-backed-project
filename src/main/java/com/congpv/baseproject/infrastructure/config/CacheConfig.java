ee5be06750b96871ec87ec4b2a86afbda3ecab24
bob <congphma2595@gmail.com>
2023-08-17 22:33:31 +0700

    add caffeine cache

--- src/main/java/com/congpv/baseproject/infrastructure/config/CacheConfig.java
+++ src/main/java/com/congpv/baseproject/infrastructure/config/CacheConfig.java
@@ -1,10 +1,14 @@
 package com.congpv.baseproject.infrastructure.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import io.lettuce.core.RedisChannelWriter;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -18,7 +22,9 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 @Configuration
 public class CacheConfig extends CachingConfigurerSupport implements CachingConfigurer {
   private final int CACHE_REDIS_TTL = 60;
-  public static final String CACHE_REDIS ="CACHE_REDIS";
+  private final int CACHE_LOCAL_TTL = 120;
+  public static final String CACHE_REDIS = "CACHE_REDIS";
+  public static final String CACHE_LOCAL = "CACHE_LOCAL";
 
 
   public RedisCacheConfiguration buildCacheConfig(int seconds) {
@@ -30,12 +36,23 @@ public class CacheConfig extends CachingConfigurerSupport implements CachingConf
                 new GenericJackson2JsonRedisSerializer()));
   }
   @Bean
-  public RedisCacheWriter redisChannelWriter(RedisConnectionFactory redisConnectionFactory) {
+  public RedisCacheWriter redisCacheWriter(RedisConnectionFactory redisConnectionFactory) {
     return RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
   }
   @Bean(name = CACHE_REDIS)
   @Primary
   public RedisCacheManager redisCacheManager(RedisCacheWriter redisCacheWriter) {
     return new RedisCacheManager(redisCacheWriter, buildCacheConfig(CACHE_REDIS_TTL));
+  }
+  @Bean(name = CACHE_LOCAL)
+  public CacheManager localCacheManager() {
+    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+    caffeineCacheManager.setCaffeine(
+        Caffeine.newBuilder()
+            .initialCapacity(50)
+            .maximumSize(3000)
+            .expireAfterWrite(CACHE_LOCAL_TTL, TimeUnit.SECONDS)
+    );
+    return caffeineCacheManager;
   }
 }
